b3baedbb4be89a2d31900acb90affc72778fba40
DavidRZuluaga <git@davidrzuluaga.com>
2018-12-09 22:44:06 -0500

    responsive design

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -10,18 +10,25 @@ class PageController < ApplicationController
   def simbolos
   end
 
-  def galeria  
-    @photos = []
-    @errormsg = ""
-    begin
-      url = "https://graph.facebook.com/v3.2/2210627005623617/photos?access_token=EAAEoct4isjABAOXwaRlnOcbpOkZAozZC4BllKDrmkqCKBGUYfnj46cL5BiC7ZA5ywAWrsv8q3UV0m54ZAZAlpm5BLLOlmGhZCZCrC5swIf7ffzZBSdexvarlJ7BZChqUb742ycvAy4hfLJNEnkSsIc962Jf5kIFKZARBkMmJRZCrzNolwZDZD"
-      resp = Net::HTTP.get_response(URI.parse(url))
-      response = JSON.parse(resp.body)
-      facebookapi = response.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
-      facebookapi[:data].each {|p| @photos << p.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}}
-    rescue StandardError => @photos
-      @photos = [{:id => "Error"}]
-      @errormsg = "Error"
+  def galeria
+    galerysource = "fbapi"
+    fbpageid = 2210627005623617
+    access_token = "EAAEoct4isjABAOXwaRlnOcbpOkZAozZC4BllKDrmkqCKBGUYfnj46cL5BiC7ZA5ywAWrsv8q3UV0m54ZAZAlpm5BLLOlmGhZCZCrC5swIf7ffzZBSdexvarlJ7BZChqUb742ycvAy4hfLJNEnkSsIc962Jf5kIFKZARBkMmJRZCrzNolwZDZD"
+    if galerysource == "fbapi"
+      @photos = []
+      @errormsg = ""
+      begin
+        url = "https://graph.facebook.com/v3.2/#{fbpageid}/photos?access_token=#{access_token}"
+        resp = Net::HTTP.get_response(URI.parse(url))
+        response = JSON.parse(resp.body)
+        facebookapi = response.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+        facebookapi[:data].each {|p| @photos << p.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}}
+      rescue StandardError => @photos
+        @photos = [{:id => "Error"}]
+        @errormsg = response
+      end
+    else
+      @photos
     end
   end
 
