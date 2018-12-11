28c3aea7d702c0c289317cd1f9f2e5681da8b4f7
DavidRZuluaga <git@davidrzuluaga.com>
2018-12-11 00:10:26 -0500

    avoid regstration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_scope :user do
+    get "/sign_in" => "devise/sessions#new"
+    #get "/sign_up" => "devise/registrations#new", as: "new_user_registration"
+  end
+  devise_for :users, :skip => [:registrations] 
+  as :user do
+  get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+  put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
   get 'admin/index'
   root 'page#index'
   get '/nosotros', to: 'page#nosotros', as: 'nosotros'
